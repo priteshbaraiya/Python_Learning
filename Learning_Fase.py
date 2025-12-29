2d74912757eae07cf8b564a11b1948ff9a06fd09
priteshbaraiya <priteshbaraiya75@gmail.com>
2025-12-29 12:35:27 +0530

    variables & data type

---
+++ Learning_Fase.py
@@ -0,0 +1,53 @@
+# Our First Program
+#print("Hello World !")
+
+# print is a input function 
+#print("Frist Programm Done")
+#age = input("Enter Your Age:")
+# print("your Age Is : "+ age)
+# print(20+10)
+
+
+# VARIABLES DECLARED AND UNDERSTAND THROW BELOW EXAMPLE 
+
+# name = "BUNTY" # string (word , centence , multiple line) 
+# age = 20       # intinger 
+# price = 20.2  # float(decimle number )
+
+# print("My Name Is : ",name )    
+# print("My age Is : ",age  )    
+
+# b = 10
+# a = b 
+# print(a)
+# print(b)
+
+
+# print(type(name))
+# print(type(age))
+# print(type(price))  
+
+
+# single , double , triple Quotes Are Valid 
+
+# name = 'PR'
+# name1 = "PR"
+# name2 = '''PR'''
+
+# print(name)
+# print(name1)
+# print(name2)
+
+# old = False 
+# b = None 
+
+# print(type(old))
+# print(type(b))
+
+
+# print sum of 2 numbers
+
+a = 5000
+b = 1000
+diff= a - b
+print(diff)
